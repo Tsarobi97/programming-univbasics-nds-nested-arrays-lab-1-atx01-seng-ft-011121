96d9b82030214619d8d18cef57de92b421415bf7
Robert Obi <robobi97@googlemail.com>
2020-12-31 23:33:44 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -39,7 +39,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  assembled_matrix[1][1]
+  assembled_matrix[row][column]
   
   
   # Given any matrix (array of arrays), a row index and a column index, 
